07550e79a2ddaa9ff480cde116127bed6db28a3c
Benjamin King <ben.king@ripjar.com>
2017-02-04 12:59:44 +0000

    Server app backups file data

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
+import fs from "fs";
 import makeStore from "./store";
 import startServer from "./server";
 import { loadRecipesAction } from "./actions";
+
+
+fs.createReadStream(__dirname + "/data/test.json").pipe(fs.createWriteStream(__dirname + "/data/test.json.bck"));
 
 export const store = makeStore();
 startServer(store);
 
 store.dispatch(loadRecipesAction(require("./data/test.json")));
 
-console.log(store.getState());
+console.log("Store loaded successfully");
+
+//process.on("exit", __onExit);
+
+//process.on("SIGTERM", __onExit);
+process.on("SIGINT", __onExit);
+
+function __onExit() {
+  console.log("Saving store...");
+  const state = store.getState();
+  fs.writeFileSync(__dirname + "/data/test.json", JSON.stringify(state, null, 4), {
+    encoding: "utf8"
+  });
+  process.exit(0);
+}
